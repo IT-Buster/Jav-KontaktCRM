9618be0bba8295e4d6bbf6b0770173760eada718
bkamzelski <brk@pentacomp.pl>
2016-08-08 11:09:23 +0200

    OK

--- Kontakt2016/src/Test.java
+++ Kontakt2016/src/Test.java
@@ -1,28 +1,41 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 
 public class Test {
-    JFrame frame;
 
-    public Test() {
-        JButton button = new JButton("exit");
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-            }
-        });
-        JPanel panel = new JPanel();
-        panel.add(button);
-        frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(panel);
-        frame.pack();
-        frame.setLocation(200,200);
-        frame.setVisible(true);
-    }
+	private JFrame frame;
 
-    public static void main(String[] args) {
-        new Test();        
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test window = new Test();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Test() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 }
